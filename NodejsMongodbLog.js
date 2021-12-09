1738fae3cb85624e16ce395d1e6f2c02eca8d2cc
yedada <yjd404.my@qq.com>
2021-12-08 17:01:40 -0800

    Install express sucessfully

---
+++ NodejsMongodbLog.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+
+app.use('/',express.static('public'))
+
+app.get("/input",(req,res)=>{
+    res.send(req.query)
+    console.log(req.query)
+})
+app.listen(10133)
