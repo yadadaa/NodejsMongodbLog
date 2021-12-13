30ded2f18e1af8f80062ffccdcf0f67e6a2f6fb1
yedada <yjd404.my@qq.com>
2021-12-12 17:25:33 -0800

    mongodb success

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test1');
+mongoose.connect('mongodb://172.21.2.236:27017/190110910133');
 const schema = {
     name: String,
     age:Number,
@@ -12,8 +12,6 @@ const schema = {
 
 }
 const mydata = mongoose.model('cat1s=', schema);
-const kitty = new mydata({ name: 'testZildjian2' });
-kitty.save()
 
 
 app.use('/',express.static('public'))
@@ -21,5 +19,8 @@ app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+    const kitty = new mydata({ name: req.query.first,health:req.query.second });
+    kitty.save()
+
 })
 app.listen(10133)
